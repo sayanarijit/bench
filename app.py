34489579ebcb51d775ad2fce4a9d46411184510a
Arijit Basu <sayanarijit@gmail.com>
2023-12-29 19:48:59 +0530

    Remove orm

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from typing import List
 
 import peewee
 import sqlalchemy as sa
-import sqlalchemy.orm
 from fastapi import FastAPI
 from piccolo import columns as picols
 from piccolo.table import Table as PiccoloTable
 from sqla_fancy_core import TableFactory
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine
 
 import piccolo_conf
 
@@ -35,13 +34,10 @@ sqla_psycopg2_engine = sa.create_engine(
     "postgresql+psycopg2://dev:dev@localhost/postgres"
 )
 
-sqla_psycopg2_sessionmaker = sqlalchemy.orm.sessionmaker(sqla_psycopg2_engine)
 
 sqla_asyncpg_engine = create_async_engine(
     "postgresql+asyncpg://dev:dev@localhost/postgres"
 )
-
-sqla_asyncpg_sessionmaker = async_sessionmaker(sqla_asyncpg_engine)
 
 
 class TaskSqla:
@@ -83,30 +79,41 @@ def startup():
 
 @app.get("/sqla-asyncpg", response_model=List[TaskDTO])
 async def list_sqla_asyncpg():
-    async with sqla_asyncpg_sessionmaker() as session:
+    async with sqla_asyncpg_engine.begin() as session:
         result = await session.execute(sa.select(TaskSqla.Table))
         return list(result.mappings())
 
 
 @app.get("/sqla-asyncpg/{id}", response_model=TaskDTO)
 async def get_sqla_asyncpg(id: int):
-    async with sqla_asyncpg_sessionmaker() as session:
+    async with sqla_asyncpg_engine.begin() as session:
         result = await session.execute(
             sa.select(TaskSqla.Table).where(TaskSqla.id == id)
         )
         return next(result.mappings())
 
 
+@app.get("/sqla-asyncpg-create/", response_model=int)
+async def create_sqla_asyncpg(name: str) -> int:
+    async with sqla_asyncpg_engine.begin() as session:
+        result = await session.execute(
+            sa.insert(TaskSqla.Table)
+            .values({TaskSqla.name: name, TaskSqla.completed: True})
+            .returning(TaskSqla.id)
+        )
+        return result.scalar_one()
+
+
 @app.get("/sqla-psycopg2", response_model=List[TaskDTO])
 def list_sqla_psycopg2():
-    with sqla_psycopg2_sessionmaker() as session:
+    with sqla_psycopg2_engine.begin() as session:
         result = session.execute(sa.select(TaskSqla.Table))
         return list(result.mappings())
 
 
 @app.get("/sqla-psycopg2/{id}", response_model=TaskDTO)
 def get_sqla_psycopg2():
-    with sqla_psycopg2_sessionmaker() as session:
+    with sqla_psycopg2_engine.begin() as session:
         result = session.execute(sa.select(TaskSqla.Table).where(TaskSqla.id == 1))
         return next(result.mappings())
 
@@ -125,6 +132,14 @@ def get_peewee(id: int):
         with peeweedb.atomic():
             result = TaskPeewee.select().where(TaskPeewee.id == id)
             return result.dicts()[0]
+
+
+@app.get("/peewee-create/", response_model=int)
+def create_peewee(name: str) -> int:
+    with peeweedb.transaction():
+        with peeweedb.atomic():
+            result = TaskPeewee.insert(name=name, completed=False).execute()
+            return result
 
 
 @app.get("/piccolo", response_model=List[TaskDTO])
