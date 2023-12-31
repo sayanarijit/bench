92a75eacf711e7d2798d73562253f14a0b50fac8
Arijit Basu <sayanarijit@gmail.com>
2023-12-31 13:15:06 +0530

    Typed Pypika

--- app.py
+++ app.py
@@ -93,9 +93,9 @@ asyncpgdb = configure_asyncpg(
 
 class TaskPypika:
     Table = pypika.Table("task")
-    id = Table.id
-    name = Table.name
-    completed = Table.completed
+    id: pypika.Column = Table.id
+    name: pypika.Column = Table.name
+    completed: pypika.Column = Table.completed
 
 
 # --------------------------------------------------------------------------------------
@@ -128,14 +128,14 @@ async def get_asyncpg(id: int, db=Depends(asyncpgdb.atomic)):
 @app.get("/pypika-asyncpg", response_model=List[TaskDTO])
 async def list_pypika_asyncpg(db=Depends(asyncpgdb.atomic)):
     q = pypika.Query.from_(TaskPypika.Table).select("*")
-    result = await db.fetch(q.get_sql())
+    result = await db.fetch(str(q))
     return [dict(row) for row in result]
 
 
 @app.get("/pypika-asyncpg/{id}", response_model=TaskDTO)
 async def get_pypika_asyncpg(id: int, db=Depends(asyncpgdb.atomic)):
     q = pypika.Query.from_(TaskPypika.Table).select("*").where(TaskPypika.id == id)
-    result = await db.fetch(q.get_sql())
+    result = await db.fetch(str(q))
     return dict(result[0])
 
 
